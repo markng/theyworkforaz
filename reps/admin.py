b22f55035b60928683fa5b12bd29890e6b2fa123
Mark Ng <mng@caritas.local>
2010-01-26 13:12:16 -0700

    add in admin config

---
+++ reps/admin.py
@@ -0,0 +1,12 @@
+from django.contrib.gis import admin
+from models import District
+from models import Representative
+from models import Party
+from models import Vote
+from models import Bill
+
+admin.site.register(District, admin.OSMGeoAdmin)
+admin.site.register(Representative)
+admin.site.register(Party)
+admin.site.register(Vote)
+admin.site.register(Bill)
