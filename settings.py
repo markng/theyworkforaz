af5fe949bd8b4f8538d70c46939d263d9a5b1629
Mark Ng <mark@markng.co.uk>
2010-01-27 08:48:04 -0700

    use local templates directory

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 # Django settings for theyworkforaz project.
+import os
+PROJECT_PATH = os.path.abspath(os.path.split(__file__)[0])
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -39,6 +41,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates')
 )
 
 INSTALLED_APPS = (
