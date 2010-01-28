d0eab0484c64266d3c4b7562e985941300a0e683
Mark Ng <mark@markng.co.uk>
2010-01-28 10:58:27 -0700

    google maps, polygon shapes + pointer for session location first pass for district page.

--- reps/views.py
+++ reps/views.py
@@ -5,6 +5,8 @@ from geocoders.google import geocoder
 from models import District
 from forms import WhereForm
 from django.contrib.gis.geos import Point
+from django.contrib.gis.maps.google.overlays import GPolygon
+from django.contrib.gis.maps.google.gmap import GoogleMap
 
 def home(request):
     """home page"""
@@ -13,6 +15,7 @@ def home(request):
         if form.is_valid():
             geocode = geocoder(settings.GOOGLE_MAPS_API_KEY, lonlat=True)
             location = geocode(form.cleaned_data['where'])
+            request.session['location'] = location
             try:
                 district = District.objects.get(area__contains=Point(location[1]))
             except District.DoesNotExist, e:
@@ -23,9 +26,19 @@ def home(request):
 
     return render_to_response('index.html', { 'form' : form, })
 
-def district(request, district=None):
+def district(request, district_id=None):
     """district page"""
-    return render_to_response('district.html')
+    totemplate = {}
+    district = District.objects.get(id=district_id)
+    totemplate['district'] = district
+    poly = GPolygon(district.area[0])
+    slocation = request.session.get('location', False)
+    if slocation and district.area.contains(Point(slocation[1])):
+        gmap = GoogleMap(polygons=[poly], markers=[Point(slocation[1])])
+    else:
+        gmap = GoogleMap(polygons=[poly])
+    totemplate['gmap'] = gmap
+    return render_to_response('district.html', totemplate)
 
 def senator(request):
     """senator page"""
