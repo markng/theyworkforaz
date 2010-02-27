be3175b85a2acace77f5c49ea643177f377d43c0
Mark Ng <mark@markng.co.uk>
2010-02-27 00:05:18 -0700

    added House

--- reps/admin.py
+++ reps/admin.py
@@ -4,9 +4,11 @@ from models import Representative
 from models import Party
 from models import Vote
 from models import Bill
+from models import House
 
 admin.site.register(District, admin.OSMGeoAdmin)
 admin.site.register(Representative)
 admin.site.register(Party)
 admin.site.register(Vote)
 admin.site.register(Bill)
+admin.site.register(House)
