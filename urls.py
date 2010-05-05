ba2a93d57ac6b98fdb00902b70aeaadd8713c001
Mark Ng <mark@markng.co.uk>
2010-05-05 13:23:19 -0700

    fix up settings and os

--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
+import os
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib.gis import admin
@@ -12,7 +14,7 @@ urlpatterns = patterns('',
     # to INSTALLED_APPS to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
-    (r'^images/(.*)$', 'django.views.static.serve', {'document_root': os_path.join(settings.PROJECT_PATH, 'media/images')}, 'images'),
-    (r'^stylesheets/(.*)$', 'django.views.static.serve', {'document_root': os_path.join(settings.PROJECT_PATH, 'media/stylesheets')}),
+    (r'^images/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.PROJECT_PATH, 'media/images')}, 'images'),
+    (r'^stylesheets/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(settings.PROJECT_PATH, 'media/stylesheets')}),
     (r'^', include('reps.urls')),
 )
