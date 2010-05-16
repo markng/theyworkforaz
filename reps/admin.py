cfbbf5056a72c43c841dc3bd769651afc3dd8ae6
Mark Ng <mark@markng.co.uk>
2010-05-16 16:46:19 -0700

    add BillDocument admin

--- reps/admin.py
+++ reps/admin.py
@@ -1,4 +1,5 @@
-from django.contrib.gis import admin
+from django.contrib.gis import admin as geoadmin
+from django.contrib import admin
 from models import District
 from models import Representative
 from models import Party
@@ -7,10 +8,13 @@ from models import Bill
 from models import House
 from models import BillDocument
 
-admin.site.register(District, admin.OSMGeoAdmin)
+class BillDocumentAdmin(admin.ModelAdmin):
+    list_filter = ('type',)
+
+admin.site.register(District, geoadmin.OSMGeoAdmin)
 admin.site.register(Representative)
 admin.site.register(Party)
 admin.site.register(Vote)
 admin.site.register(Bill)
 admin.site.register(House)
-admin.site.register(BillDocument)
+admin.site.register(BillDocument, BillDocumentAdmin)
