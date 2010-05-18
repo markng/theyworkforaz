f23b5f81352bd930341540dd2cd70164e774c141
Mark Ng <mark@markng.co.uk>
2010-05-17 20:25:30 -0700

    change to urls

--- settings.py
+++ settings.py
@@ -35,7 +35,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'theyworkforaz.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
