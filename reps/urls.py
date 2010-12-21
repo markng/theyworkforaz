9e0f5a37aaef287d112e7c758f2e2dcfeb3dac74
nmil <nmil@jrn206m3.mobile.asu.edu>
2010-12-21 16:13:55 -0700

    added the sessions.html in templates

--- reps/urls.py
+++ reps/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import *
+from models import Session
+
+sessionquery = Session.objects.all()
 
 urlpatterns = patterns('',
     (r'^$', 'reps.views.home', {}, 'home'),
@@ -6,4 +9,13 @@ urlpatterns = patterns('',
     (r'^senator/(?P<representative_id>\d+)$', 'reps.views.senator', {}, 'senator'),
     (r'^member/(?P<representative_id>\d+)$', 'reps.views.housemember', {}, 'senator'),
     (r'^bill/(?P<bill_id>.*)$', 'reps.views.bill', {}, 'bill'),
+    (
+        r'^session', 
+        'django.views.generic.list_detail.object_list', 
+        {
+            'queryset' : sessionquery,
+            'template_name' : 'sessions.html',
+            'template_object_name' : 'session',
+        }, 
+        'sessions'),
 )
