935f58b1a2f2ebc0cd01beb8a0767749c7962ef3
markng <mark@markng.co.uk>
2011-04-12 13:09:59 -0700

    Realtime indexing.

--- reps/search_indexes.py
+++ reps/search_indexes.py
@@ -2,19 +2,19 @@ from haystack.indexes import *
 from haystack import site
 from theyworkforaz.reps.models import *
 
-class DistrictSearchIndex(ModelSearchIndex):
+class DistrictSearchIndex(ModelSearchIndex, RealTimeSearchIndex):
     class Meta:
         pass
 
-class RepresentativeSearchIndex(ModelSearchIndex):
+class RepresentativeSearchIndex(ModelSearchIndex, RealTimeSearchIndex):
     class Meta:
         pass
 
-class PlaceSearchIndex(ModelSearchIndex):
+class PlaceSearchIndex(ModelSearchIndex, RealTimeSearchIndex):
     class Meta:
         pass
 
-class BillSearchIndex(ModelSearchIndex):
+class BillSearchIndex(ModelSearchIndex, RealTimeSearchIndex):
     class Meta:
         pass
 
